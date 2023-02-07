1555089e5db2f59b838b04f7a576b910ce3f7efb
marpond <sonkoly.marci02@gmail.com>
2023-02-07 11:55:18 +0100

    initial commit

---
+++ CineMoviesWeb/Models/ApiRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace CineMoviesWeb.Models;
+
+public class ApiRequest
+{
+    private static readonly HttpClient _client = new();
+    private const string URL = "https://easvcinemovies.azurewebsites.net/";
+    
+    public static async Task<string?> Greet()
+    {
+        var response = await _client.GetStringAsync(URL+nameof(Greet));
+        Console.WriteLine(response);
+        return response;
+    }
+}
