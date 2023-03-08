276d6c4f1051727236df5859d9adf6ea229c8702
Marpond <89220481+Marpond@users.noreply.github.com>
2023-03-08 15:18:52 +0100

    created user registration and login basics (#1)

---
+++ CineMoviesWeb/Models/PasswordHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace CineMoviesWeb.Models;
+
+abstract class PasswordHelper {
+    
+    public static string HashPassword(string password) {
+        return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+    }
+
+    public static bool ComparePassword(string enteredPassword, string hashedPassword) {
+        return BCrypt.Net.BCrypt.EnhancedVerify(enteredPassword, hashedPassword);
+    }
+    
+}
