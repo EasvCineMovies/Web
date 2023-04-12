ec36700b4394ae7a830385daea72b2cebf8a7249
Patrick Schemel <Patrick@schemel.dk>
2023-04-12 13:22:36 +0200

    Added k6 testing

---
+++ CineMoviesWeb/k6Tests.js
@@ -0,0 +1,15 @@
+﻿
+import http from 'k6/http'
+
+export const options = {
+    threshold: {
+        http_req_failed: ['rate<0.01'],
+        http_req_duration: ['p(95)<200']
+    },
+    duration: '5s',
+    vus: 50
+}
+
+export default function () {
+    http.get('https://easvcinemovies.azurewebsites.net/')
+}
